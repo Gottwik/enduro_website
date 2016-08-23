53a5857df8a24203a8bf9d71c1d109d54b739d30
Martin Gottweis <gottwik@gmail.com>
2016-08-23 17:21:29 +0200

    removed juicebox

--- juicebox/cms/.settings.js
+++
@@ -1,6 +0,0 @@
-{
-	settings: {
-		$admin_background_image_type: 'image',
-		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1470314017_gfvs1nmg5ut7ar42njyvi.png'
-	}
-}
