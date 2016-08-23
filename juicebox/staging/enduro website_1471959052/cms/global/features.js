25b72ba6def8108e4b71a5b07f4963955893a91f
Martin Gottweis <gottwik@gmail.com>
2016-08-23 17:20:23 +0200

    context test

---
+++ juicebox/staging/enduro website_1471959052/cms/global/features.js
@@ -0,0 +1,84 @@
+{
+	features: [
+		{
+			icon: 'keyboard-o',
+			title: 'Built for speed',
+			text: 'Clicking is lame. I wanted to build a cms where exactly zero clicks is necessary to build the whole project.'
+		},
+		{
+			icon: 'eye',
+			title: 'Killer admin ui',
+			text: 'Somebody non-technical will eventually manage content on the website. You might as well provide this person with awesome, beautiful, quick, simple experience.'
+		},
+		{
+			icon: 'fighter-jet',
+			title: 'Blazing fast',
+			text: 'Slow pageloads are the worst. Enduro pregenerates pages to provide the fastest possible response time. Coupled with direct uploading to cdn - Can\'t be faster.'
+		},
+		{
+			icon: 'gears',
+			title: 'Build tool',
+			text: 'Sass? check. Spritessheet generation? check. Enduro comes with set of build tools to build everything on the server. Forget about ftping built files.'
+		},
+		{
+			icon: 'magic',
+			title: 'Live reload',
+			text: 'Enduro comes with browsersync build in and everything hooked up. Change the color in sass file - BAM - 0.1 seconds later you see the new color in your browser.'
+		},
+		{
+			icon: 'random',
+			title: 'Code separation',
+			text: 'Enduro is built around handlebars. Logiless templates. No way to spagetticode it now.'
+		},
+		{
+			icon: 'magnet',
+			title: 'Shared templates',
+			text: 'The handlebars templates, parts of html, that enduro uses to build your site are also precompiled and accessible by javascript on client. Wow.'
+		},
+		{
+			icon: 'bolt',
+			title: 'Isomorphic javascript',
+			text: 'Using javascript for both frontend and backend is just an great idea. With Enduro, all your javascript modules can now be loaded by page generation, api and frontend code.'
+		},
+		{
+			icon: 'github',
+			title: 'Deploy by git',
+			text: 'What do you need to deploy the project? Git url. Thats it. Everything is set up. Server will build everything with enduro.'
+		},
+		{
+			icon: 'cloud',
+			title: 'Scalable',
+			text: 'Enduro is super efficient and fast. Also enduro project are selfcontained and ready to be scaled with push of a button.'
+		},
+		{
+			icon: 'database',
+			title: 'Flat files',
+			text: 'No database. Just tiny flat files. Ready to be edited by your favorite text editor(sublime, right?). Incredibly convenient, ready to be stored in git. No setup required.'
+		},
+		{
+			icon: 'cubes',
+			title: 'Rapid api',
+			text: 'Enduro comes with a simple framework built on top of express to build api endpoints. This way you can add mongodb and complex functionality to your website.'
+		},
+		{
+			icon: 'heart',
+			title: 'Simple',
+			text: 'Learning curve so flat it is actually going down. No setup, no magic. Enduro comes with a scaffolding to set you up and get you running instantly.'
+		},
+		{
+			icon: 'language',
+			title: 'Multilingual',
+			text: 'So you are making a website that will be published in multiple languages? Built in. Out of the box. No setup.'
+		},
+		{
+			icon: 'euro',
+			title: 'Open source',
+			text: 'Enduro is open-source and free. No `gold partner` bullshit either. Just honest, made with love, product.'
+		},
+		{
+			icon: 'html5',
+			title: 'Good parts',
+			text: 'Only the best ingredients were used when making Enduro. Handlebars, Sass, Gulp, Bootstrap, Angularjs, Browsersync, Express, Jasmine, Git, Npm and Heroku'
+		}
+	]
+}
---
+++ juicebox/staging/enduro website_1471959052/cms/index.js
@@ -0,0 +1,15 @@
+{
+	background_video: 'https://s3-us-west-2.amazonaws.com/coverr/mp4/We-Work-We-Wait.mp4',
+	$background_video_poster_type: 'image',
+	background_video_poster: 'https://s3-us-west-2.amazonaws.com/coverr/poster/We-Work-We-Wait.jpg',
+	heading: 'Enduro.js',
+	tagline: 'Minimalistic, lean & mean, node.js cms',
+	features_heading: 'Why enduro?',
+	Superhearoes: [
+		{
+			name: 'Batman',
+			$image_type: 'image',
+			image: ''
+		}
+	]
+}
