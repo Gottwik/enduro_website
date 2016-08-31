a303df79b17c79b1541f29ab74fe1e972fec9032
Martin Gottweis <gottwik@gmail.com>
2016-08-31 13:43:11 +0200

    changed code background color

--- cms/index.js
+++ cms/index.js
@@ -1,5 +1,17 @@
 {
-	background_video: 'https://s3-us-west-2.amazonaws.com/coverr/mp4/We-Work-We-Wait.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/42802428-32ec-4e93-bc6d-2a573da62c17/12323808.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/cf8bb811-cdcf-45c5-a313-025db281f290/16106739.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/21cf58a7-6977-4915-bc22-64fd49fecc4f/9678941.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/26b922c7-d04f-446d-a6db-3e3ee5addb1a/9340813.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/01fc87a6-74c6-4f74-b95e-1fb0afd13bd2/9554072.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/13afd6f8-6802-11e3-826a-005056923f83/6431441.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/725109.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/3469563.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/1615834.mp4',
+	// background_video: 'https://0.s3.envato.com/h264-video-previews/b2fc110c-2a29-11e3-ba2a-005056923f83/5736864.mp4',
+	background_video: '',
+
+
 	$background_video_poster_type: 'image',
 	background_video_poster: 'https://s3-us-west-2.amazonaws.com/coverr/poster/We-Work-We-Wait.jpg',
 	heading: 'Enduro.js',
