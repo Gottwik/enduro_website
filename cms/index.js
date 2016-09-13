a2e1e3d982a8a0256a601d6ec54318d5f470eaed
Martin Gottweis <gottwik@gmail.com>
2016-09-13 08:44:27 +0200

    removed fixed screen from homepage

--- cms/index.js
+++ cms/index.js
@@ -18,7 +18,7 @@
 
 
 	$background_video_poster_type: 'image',
-	background_video_poster: 'https://s3-us-west-2.amazonaws.com/coverr/poster/We-Work-We-Wait.jpg',
+	background_video_poster: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1473748977_videobg.jpg',
 	heading: 'Enduro.js',
 	tagline: 'Minimalistic, lean & mean, node.js cms',
 	features_heading: 'Why enduro?',
