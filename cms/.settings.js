2942d2c2e0fb8a30144fdc069235ceb172cf89db
Martin Gottweis <gottwik@gmail.com>
2017-01-03 11:55:10 +0100

    added press page

--- cms/.settings.js
+++ cms/.settings.js
@@ -1,7 +1,7 @@
 {
 	settings: {
 		$admin_background_image_type: 'image',
-		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1472212384_skyline-buildings-new-york-skyscrapers.jpg',
+		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1483440842_ly81yaxl6vh6ueze61or.png',
 		dark: true
 	}
 }
---
+++ cms/press.js
@@ -0,0 +1,20 @@
+{
+	press_items: [
+		{
+			$file_type: 'image',
+			file: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1483439572_enduro_e.png',
+			$preview_image_type: 'image',
+			preview_image: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1483439993_wb01vp9sr2dl2pam7vi.png',
+			name: 'Small \'E\' in hexagon',
+			filetype: 'png'
+		},
+		{
+			$file_type: 'image',
+			file: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1483439682_logo.svg',
+			$preview_image_type: 'image',
+			preview_image: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1483439978_ky0mjwht2rkvyj5rk9.png',
+			name: 'Full enduro logo',
+			filetype: 'svg'
+		}
+	]
+}
