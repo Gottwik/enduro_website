d54b04b254a6ecdcd4fecddd910a4f50c3981aab
Martin Gottweis <gottwik@gmail.com>
2017-01-03 22:53:32 +0100

    added twitter box to footer

--- cms/.settings.js
+++ cms/.settings.js
@@ -1,7 +1,7 @@
 {
 	settings: {
 		$admin_background_image_type: 'image',
-		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1483440842_ly81yaxl6vh6ueze61or.png',
+		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1483441101_s42y3l70cgxybziod2t9.png',
 		dark: true
 	}
 }
