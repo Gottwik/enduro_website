3317d393a16b77189df071856d39b15429b42e59
Martin Gottweis <gottwik@gmail.com>
2017-01-16 14:01:55 +0100

    optimized blog bg image

--- cms/.settings.js
+++ cms/.settings.js
@@ -1,7 +1,7 @@
 {
 	settings: {
 		$admin_background_image_type: 'image',
-		admin_background_image: 'https://s3-eu-west-1.amazonaws.com/enduro.website/direct_uploads/1483441101_s42y3l70cgxybziod2t9.png',
+		admin_background_image: 'http://www.thepaintershandbook.com/wp-content/uploads/2016/10/Wallpaper-Sunset-Winter.jpg',
 		dark: true
 	}
 }
